0e1c9c3618dec247f43234516b30bee25c9f5655
Amy Leung <leungmya@gmail.com>
2024-04-19 17:37:34 +0100

    feat: add test and code for test 2.1

--- spec/airport.test.js
+++ spec/airport.test.js
@@ -33,3 +33,25 @@ console.log(`==================`);
 
 // Clean Up
 afterEach();
+
+// ## User Story 2 Test 2.1 ##
+//* isAirportFull returns false when airport is not full
+// Arrange
+expected = false;
+
+// Act
+actual = dfAirport.isAirportFull();
+
+// Assert
+result = assertEquals(actual, expected);
+
+//report
+console.log(`Test 1`);
+console.log(`==================`);
+console.log("isAirportFull returns false when airport is not full");
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
--- src/airport.js
+++ src/airport.js
@@ -1,11 +1,21 @@
 class airport {
-  constructor() {
-    this.name = "Digital Futures Airport";
-    this.capacity = 10;
+    constructor() {
+        this.name = "Digital Futures Airport";
+        this.airportCapacity = 10;
+        this.airportPlanes = [];
     }
     
+    //Get maximum number of planes airport can hold
     getAirportCapacity() { 
-        return this.capacity;
+        return this.airportCapacity;
+    }
+
+    //Check to see if airport has reached maximum amount of planes allowed
+    isAirportFull() {
+        if (this.airportPlanes.length >= this.airportCapacity) { 
+            return true;
+        }
+        return false;
     }
 };
 
